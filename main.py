7ba8c3e096d7d9a6e05da8f320357a3ab2353e8c
Daniel Akio <55398457+danielchenuea@users.noreply.github.com>
2022-09-10 00:44:50 +0000

    sql and csv requests

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask
 import pandas as pd
+import sqlite3
 
 app = Flask(__name__)
 
@@ -34,12 +35,49 @@ def maior_rating_csv():
   genre_news_table = tabela[tabela['prime_genre'] == "News"]
   max_id = genre_news_table['rating_count_tot'].idxmax()
   max_row = genre_news_table.loc[[max_id]]
-  max_row.to_csv('max_row.csv', index=False)
+  resp_row = max_row[["id", 'track_name', 'size_bytes', 'price', 'prime_genre', 'rating_count_tot']]
+  resp_row.to_csv('max_row.csv', index=False)
 
-  return max_row
+  return {"message": "max_rating generated"}
+  
+@app.route('/maior_rating_sql')
+def maior_rating_sql():
+  tabela = pd.read_csv('AppleStore.csv')
+  
+  genre_news_table = tabela[tabela['prime_genre'] == "News"]
+  max_id = genre_news_table['rating_count_tot'].idxmax()
+  max_row = genre_news_table.loc[[max_id]]
 
-@app.route('/top10_bm')
-def top10_bm():
+  con = sqlite3.connect("data.db")
+  cursor = con.cursor()
+  cursor.execute("CREATE TABLE if not exists rating(\
+      id	    INTEGER UNIQUE,\
+      track_name	TEXT,\
+      size_bytes	INTEGER,\
+      price	REAL,\
+      prime_genre	TEXT,\
+      rating_count_tot	INTEGER,\
+      PRIMARY KEY(id)\
+  )")
+
+  for rows in max_row.itertuples():
+    query = "insert into rating(id, track_name,size_bytes,price,prime_genre,rating_count_tot) values (?,?,?,?,?,?)"
+    cursor.execute(query, (
+      int(rows.id), 
+      rows.track_name, 
+      int(rows.size_bytes), 
+      int(rows.price),
+      rows.prime_genre,
+      int(rows.rating_count_tot)))
+    
+  con.commit()
+  con.close()
+  
+
+  return {"message": "max_rating sql generated"}
+
+@app.route('/top10_bm_json')
+def top10_bm_json():
   tabela = pd.read_csv('AppleStore.csv')
 
   genre_media_table = tabela[tabela['prime_genre'].isin(["Music", "Books"])]
@@ -58,8 +96,55 @@ def top10_bm():
 
   return resp_rows
   
-@app.route('/top1_bm')
-def top1_bm():
+@app.route('/top10_bm_csv')
+def top10_bm_csv():
+  tabela = pd.read_csv('AppleStore.csv')
+
+  genre_media_table = tabela[tabela['prime_genre'].isin(["Music", "Books"])]
+  top10 = genre_media_table['rating_count_tot'].nlargest(n=10).index.tolist()
+  top_rows = genre_media_table.loc[top10]
+  resp_rows = top_rows[["id", 'track_name', 'size_bytes', 'price', 'prime_genre', 'rating_count_tot']]
+  resp_rows.to_csv('top10.csv', index=False)
+
+  return {"message": "top10 generated"}
+
+@app.route('/top10_bm_sql')
+def top10_bm_sql():
+  tabela = pd.read_csv('AppleStore.csv')
+  
+  genre_media_table = tabela[tabela['prime_genre'].isin(["Music", "Books"])]
+  top10 = genre_media_table['rating_count_tot'].nlargest(n=10).index.tolist()
+  top_rows = genre_media_table.loc[top10]
+
+  con = sqlite3.connect("data.db")
+  cursor = con.cursor()
+  cursor.execute("CREATE TABLE if not exists top10(\
+      id	    INTEGER UNIQUE,\
+      track_name	TEXT,\
+      size_bytes	INTEGER,\
+      price	REAL,\
+      prime_genre	TEXT,\
+      rating_count_tot	INTEGER,\
+      PRIMARY KEY(id)\
+  )")
+
+  for rows in top_rows.itertuples():
+    query = "insert into top10(id, track_name,size_bytes,price,prime_genre,rating_count_tot) values (?,?,?,?,?,?)"
+    cursor.execute(query, (
+      int(rows.id), 
+      rows.track_name, 
+      int(rows.size_bytes), 
+      int(rows.price),
+      rows.prime_genre,
+      int(rows.rating_count_tot)))
+    
+  con.commit()
+  con.close()
+  
+  return {"message": "top10 sql generated"}
+  
+@app.route('/top1_bm_json')
+def top1_bm_json():
   tabela = pd.read_csv('AppleStore.csv')
 
   genre_media_table = tabela[tabela['prime_genre'].isin(["Music", "Books"])]
@@ -78,25 +163,52 @@ def top1_bm():
 
   return resp_rows
 
+@app.route('/top1_bm_csv')
+def top1_bm_csv():
+  tabela = pd.read_csv('AppleStore.csv')
+
+  genre_media_table = tabela[tabela['prime_genre'].isin(["Music", "Books"])]
+  top1 = genre_media_table['rating_count_tot'].nlargest(n=1).index.tolist()
+  top_rows = genre_media_table.loc[top1]
+  resp_rows = top_rows[["id", 'track_name', 'size_bytes', 'price', 'prime_genre', 'rating_count_tot']]
+  resp_rows.to_csv('top1.csv', index=False)
+
+  return {"message": "top1 generated"}
+
+
+@app.route('/top1_bm_sql')
+def top1_bm_sql():
+  tabela = pd.read_csv('AppleStore.csv')
+  
+  genre_media_table = tabela[tabela['prime_genre'].isin(["Music", "Books"])]
+  top1 = genre_media_table['rating_count_tot'].nlargest(n=1).index.tolist()
+  top_rows = genre_media_table.loc[top1]
+
+  con = sqlite3.connect("data.db")
+  cursor = con.cursor()
+  cursor.execute("CREATE TABLE if not exists top1(\
+      id	    INTEGER UNIQUE,\
+      track_name	TEXT,\
+      size_bytes	INTEGER,\
+      price	REAL,\
+      prime_genre	TEXT,\
+      rating_count_tot	INTEGER,\
+      PRIMARY KEY(id)\
+  )")
+
+  for rows in top_rows.itertuples():
+    query = "insert into top1(id, track_name,size_bytes,price,prime_genre,rating_count_tot) values (?,?,?,?,?,?)"
+    cursor.execute(query, (
+      int(rows.id), 
+      rows.track_name, 
+      int(rows.size_bytes), 
+      int(rows.price),
+      rows.prime_genre,
+      int(rows.rating_count_tot)))
+    
+  con.commit()
+  con.close()
+  
+  return {"message": "top1 sql generated"}
 
 app.run(host='0.0.0.0')
-
-
-'''
-tabela = pd.read_csv('AppleStore.csv')
-
-genre_media_table = tabela[tabela['prime_genre'].isin(["Music", "Books"])]
-top10 = genre_media_table['rating_count_tot'].nlargest(n=10).index.tolist()
-top_rows = genre_media_table.loc[top10]
-resp_rows = []
-print(top_rows)
-for rows in top_rows.itertuples():
-  resp_rows.append({
-    'id': int(rows.id),
-    'track_name': rows.track_name,
-    'size_bytes': int(rows.size_bytes),
-    'price': int(rows.price),
-    'prime_genre': rows.prime_genre,
-    'rating_count_tot': int(rows.rating_count_tot)
-  })
-print(resp_rows)'''
